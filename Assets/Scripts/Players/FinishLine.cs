0a06ff59ae4c64223fe99ffcd155aa9665b09e75
Daniel_Naslund <daniel.naslund@futuregames.nu>
2020-10-19 15:48:37 +0200

    Started with finish line

---
+++ Assets/Scripts/Players/FinishLine.cs
@@ -0,0 +1,60 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    public List<GameObject> Tb;
+    
+    
+
+    private void Start()
+    {
+        Tb = new List<GameObject>();       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {           
+            if (!Tb.Contains(other.gameObject))
+            {
+                Tb.Add(other.gameObject);
+                Debug.Log("added");
+            }
+        }      
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {         
+            if (Tb.Contains(other.gameObject))
+            {
+                Tb.Remove(other.gameObject);
+                Debug.Log("remove");
+            }
+        }
+
+    }
+
+    private void Update()
+    {
+        checkAmount();
+    }
+
+    private void checkAmount()
+    {
+        if(Tb.Count >= 2)
+        {
+            
+          
+        }
+    }
+
+
+
+
+
+}
