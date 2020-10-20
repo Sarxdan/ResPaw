73e394b16c739091fe5c7283f19cd98242bf3f2f
Melon332 <thederpking@outlook.com>
2020-10-20 13:53:44 +0200

    Tweaked animations, changed so that the GameManager counts from 3 to 0 instead of vice versa, added a script to check the amount of lives each player has.

---
+++ Assets/Scripts/Players/PlayerLives.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLives : MonoBehaviour
+{
+    [SerializeField] Text P1, P2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        P1.text = "P1: " + GameManager.Instance.PlayerOneLife.ToString();
+        P2.text = "P2: " + GameManager.Instance.PlayerTwoLife.ToString();
+    }
+}
