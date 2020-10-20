fa5b35b8151919e4bbb051b37a28eee855edd0c2
Daniel_Naslund <daniel.naslund@futuregames.nu>
2020-10-20 16:52:22 +0200

    made new folder for game mechanics, basic switches made

---
+++ Assets/Scripts/Game mechanics/Switches.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    [SerializeField] Transform doorStop;
+    float doorSpeed = 1f;
+    bool isTrigger = false;
+    bool isOpen;
+    private void Start()
+    {        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            isTrigger = true;
+        }
+
+    }
+
+    private void Update()
+    {
+        if(isTrigger == true)
+        {
+            test();
+        }
+            
+    }
+
+    void test()
+    {
+        door.transform.position = Vector3.MoveTowards(door.transform.position, doorStop.position, doorSpeed * Time.deltaTime);
+    }
+
+}
