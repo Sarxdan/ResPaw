37eca3e8e02658078c362908aff1525f257b2fcc
Sandra Andersson <sandra.andersson@futuregames.nu>
2020-10-21 11:23:20 +0200

    Horizontal enemy turns around if it gets stuck when player is in the way of its border

--- Assets/Scripts/AI/HorizontalEnemy.cs
+++ Assets/Scripts/AI/HorizontalEnemy.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.Enums;
+using UnityEditor.PackageManager.Requests;
 
 //
 // Created by: Sandra Andersson
@@ -11,30 +13,62 @@ using UnityEngine;
 
 public class HorizontalEnemy : MonoBehaviour
 {
-    public float movementSpeed = 1.5f;
+    public float movementSpeed = 2.5f;
+
+    public float rayLength = 5.0f;
+    public RaycastHit[] hits;
+    public Vector3 rayDir = new Vector3(1, 0, 0);
     
     private CharacterController charContr;
     private Vector3 direction = Vector3.forward;
     private Animator anim;
+    
+    private float lastTime = 1;
+    private float lastPos;
+    private float checkSec = .2f;
+    private float distanceCheck = .02f;
 
-    // Start is called before the first frame update
+    private bool rotated = false;
+
     void Start()
     {
         anim = GetComponent<Animator>();
+        lastPos = transform.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Time.deltaTime * movementSpeed * direction);
+
+        if ((Time.time - lastTime) >= checkSec)
+        {
+            if ((((transform.position.x - lastPos)* -direction.z) < (distanceCheck * -direction.z)) && !rotated)
+            {
+                Rotate();
+            }
+            else if (rotated)
+            {
+                rotated = false;
+            }
+
+            lastPos = transform.position.x;
+            lastTime = Time.time;
+        }
+        
+    }
+
+    private void Rotate()
+    {
+        direction = -direction;
+        anim.SetTrigger((direction.z >= 1 ? "TurnLeft" : "TurnRight"));
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "AI Border")
         {
-            direction = -direction;
-            anim.SetTrigger((direction.z >= 1 ? "TurnLeft" : "TurnRight"));
+            Rotate();
+            rotated = true;
         }
     }
 
