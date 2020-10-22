23b13ee79b4b0fe3cc002ebf34ae174ebd86fc75
Melon332 <thederpking@outlook.com>
2020-10-22 12:13:21 +0200

    Added a script and added some circles in the programmer scene

---
+++ Assets/Scripts/Game mechanics/LibraPlatforms.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibraPlatforms : MonoBehaviour
+{
+    GameObject Platform1, Platform2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Platform1 = GameObject.Find("PlatForm1");
+        Platform2 = GameObject.Find("Platform2");
+
+        Collider collider1 = Platform1.GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+           Debug.Log("Hello");
+        }
+
+    }
+}
