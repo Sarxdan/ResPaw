03850b271d03f1b84be9daa0f566356bc4cbd9c8
Daniel_Naslund <daniel.naslund@futuregames.nu>
2020-10-23 14:04:09 +0200

    fixed finishline scene loading

--- Assets/Scripts/UI/LevelControll.cs
+++ Assets/Scripts/UI/LevelControll.cs
@@ -27,6 +27,6 @@ public class LevelControll : MonoBehaviour
             PlayerPrefs.SetInt("LevelPassed", sceneIndex);
             
         }
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 }
