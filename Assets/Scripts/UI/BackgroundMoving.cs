79628a9c3fb0870995d54be3ad29fea028e26f63
Daniel_Naslund <daniel.naslund@futuregames.nu>
2020-10-27 15:06:48 +0100

    Made a moving background

---
+++ Assets/Scripts/UI/BackgroundMoving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMoving : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float clamppos;
+
+    public Vector3 StartPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float newPosition = Mathf.Repeat(Time.time * speed, clamppos);
+        transform.position = StartPosition + Vector3.left * newPosition;
+    }
+}
