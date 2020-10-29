4bd946c50f070e4dfd60eca83f943df02d0e3121
Melon332 <thederpking@outlook.com>
2020-10-29 11:19:17 +0100

    Added music so that it's smooth transisiton

---
+++ Assets/Scripts/UI/GameplayMusic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayMusic : MonoBehaviour
+{
+    public static GameplayMusic Instance { get; private set; }
+    [SerializeField]
+    int sceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckIfInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        DestroyMenuMusic();
+    }
+    private void DestroyMenuMusic()
+    {
+        if (sceneIndex == 3 || sceneIndex == 4 || sceneIndex == 5 || sceneIndex == 6)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void CheckIfInstance()
+    {
+        //doing a singleton since we only want one of these in each scene.
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
