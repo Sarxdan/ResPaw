725bd0abf429d1a13c5067887b8ebcc88f77387a
Melon332 <thederpking@outlook.com>
2020-10-29 14:42:28 +0100

    Added button H and fixed tags and layers and tags.

--- Assets/Scripts/UI/ControlLayout.cs
+++ Assets/Scripts/UI/ControlLayout.cs
@@ -17,7 +17,7 @@ public class ControlLayout : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q) && !hasActivated)
+        if (Input.GetKeyDown(KeyCode.H) && !hasActivated)
         {
             StartCoroutine(showText());
         }
